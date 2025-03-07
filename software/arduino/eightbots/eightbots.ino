2700b6c9f7dd1838a9da803b652c0eb6ffebdee7
Yusei Ito <me@yuseiito.com>
2025-03-07 22:46:19 +0900

    feat: Enable arduino OTA

--- software/arduino/eightbots/eightbots.ino
+++ software/arduino/eightbots/eightbots.ino
@@ -5,6 +5,11 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <ESP32Encoder.h>
+#include <WiFi.h>
+#include <ESPmDNS.h>
+#include <NetworkUdp.h>
+#include <ArduinoOTA.h>
+
 
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
@@ -18,6 +23,9 @@ ESP32Encoder encoder;
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
+const char* ssid = WIFI_SSID;
+const char* password = WIFI_PASSWORD;
+
 void print_aa_logo(void){
 	Serial.println();
 	Serial.println(F(" ______ _       _     _   _           _       "));
@@ -29,6 +37,57 @@ void print_aa_logo(void){
 	Serial.println(F("           __/ |                              "));
 	Serial.println(F("          |___/                               "));
 	Serial.println();
+}
+
+void configure_wifi(void){
+	Serial.println(F("Configuring WiFi..."));
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Connection Failed!");
+		while(1); // TODO: Try to reconnect using a timer
+	}
+
+  Serial.print("WiFi Ready. IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void configure_ota(void){
+  ArduinoOTA
+    .onStart([]() {
+      String type;
+      if (ArduinoOTA.getCommand() == U_FLASH) {
+        type = "sketch";
+      } else {  // U_SPIFFS
+        type = "filesystem";
+      }
+
+      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+      Serial.println("Start updating " + type);
+    })
+    .onEnd([]() {
+      Serial.println("\nEnd");
+    })
+    .onProgress([](unsigned int progress, unsigned int total) {
+      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    })
+    .onError([](ota_error_t error) {
+      Serial.printf("Error[%u]: ", error);
+      if (error == OTA_AUTH_ERROR) {
+        Serial.println("Auth Failed");
+      } else if (error == OTA_BEGIN_ERROR) {
+        Serial.println("Begin Failed");
+      } else if (error == OTA_CONNECT_ERROR) {
+        Serial.println("Connect Failed");
+      } else if (error == OTA_RECEIVE_ERROR) {
+        Serial.println("Receive Failed");
+      } else if (error == OTA_END_ERROR) {
+        Serial.println("End Failed");
+      }
+    });
+
+  ArduinoOTA.begin();
+
 }
 
 
@@ -94,9 +153,12 @@ void setup() {
 	Wire.begin(PIN_SDA, PIN_SCL);
 	configure_oled();
 	configure_rotary_encoder();
+	configure_wifi();
+	configure_ota();
 }
 
 void loop() {
+  ArduinoOTA.handle();
 	Serial.print("count: ");
 	Serial.print(encoder.getCount());
 	Serial.print(" SW: ");
